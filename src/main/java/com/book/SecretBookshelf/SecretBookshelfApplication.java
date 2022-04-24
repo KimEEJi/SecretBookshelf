2f258840e32519aa6049bc0e02fcbd3951e9a9ee
cathy970816@gmail.com <cathy970816@gmail.com>
2022-04-24 20:39:41 +0900

    Create Project

---
+++ src/main/java/com/book/SecretBookshelf/SecretBookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.book.SecretBookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretBookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/SecretBookshelf/SecretBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.SecretBookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
