cbb9b89ea8bc884f661428a730f835c494def9ef
cathy970816@gmail.com <cathy970816@gmail.com>
2022-04-28 06:52:01 +0900

    postgresql 연동

--- src/main/java/com/book/SecretBookshelf/SecretBookshelfApplication.java
+++ src/main/java/com/book/SecretBookshelf/SecretBookshelfApplication.java
@@ -1,12 +1,33 @@
 package com.book.SecretBookshelf;
 
+import com.book.SecretBookshelf.postgresqltest.PostgreSQLRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class SecretBookshelfApplication {
 
 	public static void main(String[] args) {
+		String url = "jdbc:postgresql://localhost:5432/bookshelf";
+		String user = "kimeeji";
+		String password = "postgres"; //password 입력
+		try
+		{
+			Connection connect = null;
+			connect = DriverManager.getConnection(url, user, password);
+			if(connect != null) {
+				System.out.println("Connection successful!!");
+			}
+			else {
+				throw new SQLException("no connection...");
+			}
+		} catch (SQLException ex) {
+			//throw new RuntimeException();
+		}
 		SpringApplication.run(SecretBookshelfApplication.class, args);
 	}
 
---
+++ src/main/java/com/book/SecretBookshelf/postgresqltest/PostgreSQLRunner.java
@@ -0,0 +1,36 @@
+package com.book.SecretBookshelf.postgresqltest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class PostgreSQLRunner implements ApplicationRunner {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(dataSource.getConnection());
+            System.out.println(connection.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE book_product(book_no INTEGER NOT NULL, book_name VARCHAR(255), PRIMARY KEY (book_no))";
+            statement.executeUpdate(sql);
+
+        }
+        jdbcTemplate.execute("INSERT INTO book_product VALUES (1,'hi')");
+    }
+}
